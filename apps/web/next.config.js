b27513f643a30a0fa03b53c39e03773f50a125e5
SGeri <sarffygeri@gmail.com>
2023-06-02 20:51:26 +0200

    prettier & config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,17 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds and Linting.
+ */
+!process.env.SKIP_ENV_VALIDATION && import("./src/env.mjs");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  transpilePackages: ["config", "@packages/weaviate"],
+  eslint: { ignoreDuringBuilds: false },
+  typescript: { ignoreBuildErrors: false },
 };
 
 module.exports = nextConfig;
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,12 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import baseConfig from "config/tailwind/tailwind.config";
+
+export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  presets: [baseConfig],
 };
